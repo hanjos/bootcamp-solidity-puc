48ae76d26955d0e0d29eb6cccfe60a6bd3aa96b5
Humberto Anjos <h.anjos@acm.org>
2018-02-07 19:16:05 -0200

    Docs.

--- Investment/contracts/FundToken.sol
+++ Investment/contracts/FundToken.sol
@@ -14,91 +14,112 @@ contract FundToken {
   // Mint and burn
   // =============
 
-  // Called only by this or derived contracts
-  function mint(address account, uint tokens) internal {
-    require(tokens > 0);
-    require(tokenCount + tokens > tokenCount);
-    require(balances[account] + tokens > balances[account]);
+  /** 
+   * Creates `amount` FUNDs in the total supply, which are deposited in 
+   * `account`. This function is only for derived contracts, which control
+   * the token emission.
+   * 
+   * Fires a `Mint` event.
+   */
+  function mint(address account, uint amount) internal {
+    require(amount > 0);
+    require(tokenCount + amount > tokenCount);
+    require(balances[account] + amount > balances[account]);
 
-    tokenCount += tokens;
-    balances[account] += tokens;
+    tokenCount += amount;
+    balances[account] += amount;
 
-    Minted(tokens);
+    Mint(amount);
   }
 
-  // Called only by this or derived contracts
-  function burn(address account, uint tokens) internal {
-    require(tokens > 0);
-    require(tokenCount >= tokens);
-    require(balances[account] >= tokens);
+  /** 
+   * Destroys `amount` tokens from the total supply and `account`. This 
+   * function is only for derived contracts, which control the token emission.
+   * 
+   * Fires a `Burn` event.
+   */
+  function burn(address account, uint amount) internal {
+    require(amount > 0);
+    require(tokenCount >= amount);
+    require(balances[account] >= amount);
 
-    tokenCount -= tokens;
-    balances[account] -= tokens;
+    tokenCount -= amount;
+    balances[account] -= amount;
 
-    Burned(tokens);
+    Burn(amount);
   }
 
-  event Minted(uint value);
-  event Burned(uint value);
+  /// Fired when tokens are `mint`ed.
+  event Mint(uint value);
+
+  /// Fired when tokens are `burn`ed.
+  event Burn(uint value);
 
   // ==============
   // ERC20 standard
   // ==============
 
-  // How many tokens exist
+  /// Returns how many tokens exist.
   function totalSupply() public view returns (uint) {
     return tokenCount;
   }
 
-  // Get the token balance for account `_address`
+  /// Gets the token balance for `account`.
   function balanceOf(address account) public view returns (uint) {
     return balances[account];
   }
 
-  // Transfer the balance from owner's account to another account
-  function transfer(address to, uint tokens) public returns (bool) {
-    require(balances[msg.sender] >= tokens);
-    require(balances[to] + tokens > balances[to]);
+  /// Transfers `amount` tokens from the sender's account to the account `to`.
+  /// Fires a `Transfer` event.
+  function transfer(address to, uint amount) public returns (bool) {
+    require(balances[msg.sender] >= amount);
+    require(balances[to] + amount > balances[to]);
 
-    balances[msg.sender] -= tokens;
-    balances[to] += tokens;
+    balances[msg.sender] -= amount;
+    balances[to] += amount;
 
-    Transfer(msg.sender, to, tokens);
+    Transfer(msg.sender, to, amount);
     return true;
   }
 
-  // Returns how many tokens `owner` has allowed `spender` to spend in their name
+  /// Returns how many tokens `owner` has allowed `spender` to spend in their
+  /// name.
   function allowance(address owner, address spender) public view returns (uint) {
     return allowances[owner][spender];
   }
 
-  // Allow `spender` to withdraw from your account, multiple times, up to the `tokens` amount.
-  // If this function is called again it overwrites the current allowance with _value.
-  function approve(address spender, uint tokens) public returns (bool) {
-    allowances[msg.sender][spender] = tokens;
+  /**
+   * Allows `spender` to withdraw from `msg.sender`'s account, multiple times,
+   * up to `amount`. If this function is called again, it overwrites the current
+   * allowance with `amount`.
+   * 
+   * Fires an `Approval` event.
+   */
+  function approve(address spender, uint amount) public returns (bool) {
+    allowances[msg.sender][spender] = amount;
 
-    Approval(msg.sender, spender, tokens);
+    Approval(msg.sender, spender, amount);
     return true;
   }
 
-  // Send `tokens` amount of tokens from address `from` to address `to`
-  // The transferFrom method is used for a withdraw workflow, allowing contracts to send
-  // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
-  // fees in sub-currencies; the command should fail unless the _from account has
-  // deliberately authorized the sender of the message via some mechanism
-  function transferFrom(address from, address to, uint tokens) public returns (bool) {
-    require(balances[from] >= tokens);
-    require(allowances[from][to] >= tokens);
-    require(balances[to] + tokens > balances[to]);
+  /** 
+   * Send `amount` tokens from `from` to `to`. Errors if the amount hasn't
+   * been approved previously with `approve`. The maximum amount authorized 
+   * can be checked with `allowance`.
+   */
+  function transferFrom(address from, address to, uint amount) public returns (bool) {
+    require(balances[from] >= amount);
+    require(allowances[from][to] >= amount);
+    require(balances[to] + amount > balances[to]);
 
-    balances[from] -= tokens;
-    balances[to] += tokens;
-    allowances[from][to] -= tokens;
+    balances[from] -= amount;
+    balances[to] += amount;
+    allowances[from][to] -= amount;
 
-    Transfer(from, to, tokens);
+    Transfer(from, to, amount);
     return true;
   }
 
-  event Approval(address indexed owner, address indexed spender, uint tokens);
-  event Transfer(address indexed from, address indexed to, uint tokens);
+  event Approval(address indexed owner, address indexed spender, uint amount);
+  event Transfer(address indexed from, address indexed to, uint amount);
 }
